3f4c9ab8564a03ecb2d895f0ee306538615ffb3d
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-06-27 15:28:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function translate() {
+async function translate() {
     var word = document.getElementById('word').value;
 
-    // PHP code as a string
-    var phpCode = `
-        $word = '` + word + `';
-        $apiUrl = 'https://api.mymemory.translated.net/get?q=' . $word . '&langpair=en|es';
-        $response = file_get_contents($apiUrl);
-        $data = json_decode($response, true);
-        if (isset($data['responseData']['translatedText'])) {
-            echo $data['responseData']['translatedText'];
-        } else {
-            echo 'No translation found';
-        }
-    `;
+    // Fetch the PHP code from the file
+    var phpCode = await fetch('translate.php')
+        .then(response => response.text());
+
+    // Replace the placeholder with the actual word
+    phpCode = phpCode.replace('<?php', '').replace('?>', '');
+    phpCode = phpCode.replace(/\$word = \$_GET\['word'\];/, `var word = '${word}';`);
+    phpCode = phpCode.replace(/file_get_contents\((.+?)\)/g, 'await fetch($1).then(response => response.text())');
+    phpCode = phpCode.replace(/json_decode\((.+?)\)/g, 'JSON.parse($1)');
+    phpCode = phpCode.replace(/echo/g, 'return');
 
     // Transpile PHP code to JavaScript and execute
-    var php = new PHP_JS();
-    var result = php.create_scope().execute(phpCode);
+    var php = new PHP_JS().create_scope();
+    var result = await php.execute(phpCode);
 
     // Display the result
     document.getElementById('translation').innerText = result;
